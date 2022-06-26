e4633c4c9cd8f852d90989a2e271624f28f4c165
Atiqul-Isla <isla.atiqul@gmail.com>
2022-06-25 22:49:20 -0400

    Setting.py configuration

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from django.test.utils import ignore_warnings
+
+ignore_warnings(message="No directory at", module="whitenoise.base").enable()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
